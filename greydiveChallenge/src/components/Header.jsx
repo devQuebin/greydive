2ae3093a20e32938528e107f0d0c7063067db932
Quebin <matsuda.m.kevin@gmail.com>
2022-11-25 19:09:04 -0300

    opcional navBar en bootstrap

--- greydiveChallenge/src/components/Header.jsx
+++ greydiveChallenge/src/components/Header.jsx
@@ -26,3 +26,50 @@ function Header({ items }) {
   );
 }
 export default Header;
+
+/* 
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+
+    <header className="header">
+      <a href="https://www.greydive.com/">
+        <img src={logo} className="logo" alt="simulacr" />
+      </a>
+
+      <nav>
+        <ul className="nav-links">
+          {items.map((item, i) => (
+            <li key={item.idVideo}>
+              <Link className="link" to={`/tester${i + 1}`}>
+                Tester #{i + 1}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </header>
+
+<Navbar className="header" expand="lg">
+      <Container>
+        <Navbar.Brand href="https://www.greydive.com/">
+          <img src={logo} className="logo" alt="simulacr" />
+        </Navbar.Brand>
+
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <ul className="nav-links">
+              {items.map((item, i) => (
+                <li key={item.idVideo}>
+                  <Nav.Link className="link" href={`/tester${i + 1}`}>
+                    Tester #{i + 1}
+                  </Nav.Link>
+                </li>
+              ))}
+            </ul>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+*/
